4f451d3620c70d9a624ef4bf7607df608a12ac62
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-18 13:37:30 +0530

    responsive bug fix

--- src/components/HomeImg.jsx
+++ src/components/HomeImg.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomeImage from '../assets/Home_img.avif'
+import { instagram, facebook, youtube, whatsapp } from '../assets/icons/icon';
 const HomeImg = () => {
   return (
     <div className='w-full bg-black'>
@@ -9,6 +10,13 @@ const HomeImg = () => {
         <h1 className='shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'><span className='text-[#ffee00]'>Grand</span> View</h1>
         <h1 className='shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'>Resort</h1>
       </div>
+        <div className='w-full p-5 flex gap-4 absolute items-center justify-center bottom-0 '>
+          <img src={whatsapp} alt="whatsapp" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+          <img src={instagram} alt="instagram" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+          <img src={facebook} alt="facebook" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+          <img src={youtube} alt="youtube" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+        </div>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = ({ logo }) => {
     }
 
     return (
-        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-7xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
+        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-[115rem] mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
             <NavLink to="/">
                 <img src={logo} alt="logo" className='w-[233px] h-[60px] object-cover rounded-lg shadow-md home-logo' />
             </NavLink>
 
-            <nav ref={navRef} className="flex text-lg gap-10 font-medium md-device responsive_nav">
-                <div className='nav-mobile'>
+            <nav ref={navRef} className="flex text-lg gap-[10rem] font-medium md-device responsive_nav">
+                <div className='nav-mobile flex gap-10'>
                     <NavLink exact to='/' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
                         Home
                     </NavLink>
@@ -36,19 +36,14 @@ const Navbar = ({ logo }) => {
                         Blog
                     </NavLink>
                 </div>
-                <div className='flex gap-4 items-center justify-normal '>
-                    <img src={whatsapp} alt="whatsapp" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                    <img src={instagram} alt="instagram" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                    <img src={facebook} alt="facebook" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                    <img src={youtube} alt="youtube" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                </div>
-                <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn nav-close-btn absolute z-[999] top-0 right-0 m-5'>
+                
+                <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn nav-close-btn absolute z-[999] top-0 right-0 m-5 text-[1.5rem]'>
                     <FaTimes />
                 </button>
             </nav>
 
-
-            <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn'>
+            
+            <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn text-[1.5rem]'>
                 <FaBars />
             </button>
         </header>
