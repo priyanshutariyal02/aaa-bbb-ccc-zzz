22a062becab1e205b1f30f01523de6bb26803bdc
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-18 15:07:18 +0530

    fix nav auto open

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-
 import { NavLink } from 'react-router-dom';
+
 const Navbar = ({ logo }) => {
+    const navRef = useRef(null);
 
-    const navRef = useRef();
-
+    useEffect(() => {
+        if (navRef.current) {
+            navRef.current.classList.remove('responsive_nav');
+        }
+    }, []);
 
     const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
+        navRef.current.classList.toggle('responsive_nav');
+    };
 
     return (
         <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-[115rem] mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
@@ -17,7 +21,7 @@ const Navbar = ({ logo }) => {
                 <img src={logo} alt="logo" className='w-[233px] h-[60px] object-cover rounded-lg shadow-md home-logo' />
             </NavLink>
 
-            <nav ref={navRef} className="flex text-lg gap-[10rem] font-medium md-device responsive_nav">
+            <nav ref={navRef} className="flex text-lg gap-[10rem] font-medium md-device">
                 <div className='nav-mobile flex gap-10'>
                     <NavLink onClick={showNavbar} exact to='/' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
                         Home
@@ -41,12 +45,11 @@ const Navbar = ({ logo }) => {
                 </button>
             </nav>
 
-
-            <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn text-[1.5rem]'>
+            <button onClick={showNavbar} className='nav-btn bg-white rounded-xl p-3 shadow-md  text-[1.5rem]'>
                 <FaBars />
             </button>
         </header>
-    )
+    );
 }
 
 export default Navbar;
