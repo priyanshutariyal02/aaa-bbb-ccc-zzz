c558313482b73693aedab0d737eba6396bd28b52
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-18 13:10:08 +0530

    make home responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 import { NavLink } from 'react-router-dom';
 import { instagram, facebook, youtube, whatsapp } from '../assets/icons/icon';
 const Navbar = ({ logo }) => {
 
+    const navRef = useRef();
+
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
-        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-5xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
-            <NavLink to="/" className='w-[233px] h-[60px]'>
-                <img src={logo} alt="logo" className='w-[233px] h-[60px] object-cover rounded-lg shadow-md' />
+        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-7xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
+            <NavLink to="/">
+                <img src={logo} alt="logo" className='w-[233px] h-[60px] object-cover rounded-lg shadow-md home-logo' />
             </NavLink>
 
-            <nav className="flex text-lg gap-7 font-medium">
-                <NavLink exact to='/' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
-                    Home
-                </NavLink>
-                <NavLink exact to='/gallery' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
-                    Gallery
-                </NavLink>
-                <NavLink to='/about' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
-                    About
-                </NavLink>
-                <NavLink to='/contact' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
-                    Contact
-                </NavLink>
-                <NavLink to='/blog' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
-                    Blog
-                </NavLink>
+            <nav ref={navRef} className="flex text-lg gap-10 font-medium md-device responsive_nav">
+                <div className='nav-mobile'>
+                    <NavLink exact to='/' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
+                        Home
+                    </NavLink>
+                    <NavLink exact to='/gallery' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
+                        Gallery
+                    </NavLink>
+                    <NavLink to='/about' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                        About
+                    </NavLink>
+                    <NavLink to='/contact' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                        Contact
+                    </NavLink>
+                    <NavLink to='/blog' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                        Blog
+                    </NavLink>
+                </div>
+                <div className='flex gap-4 items-center justify-normal '>
+                    <img src={whatsapp} alt="whatsapp" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+                    <img src={instagram} alt="instagram" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+                    <img src={facebook} alt="facebook" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+                    <img src={youtube} alt="youtube" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
+                </div>
+                <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn nav-close-btn absolute z-[999] top-0 right-0 m-5'>
+                    <FaTimes />
+                </button>
             </nav>
 
-            <div className='flex w-[3rem] h-[3rem] gap-4 items-center justify-normal'>
-                <img src={whatsapp} alt="whatsapp" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                <img src={instagram} alt="instagram" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                <img src={facebook} alt="facebook" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-                <img src={youtube} alt="youtube" className='w-[30px] h-[30px] hover:scale-125 duration-200' />
-            </div>
+
+            <button onClick={showNavbar} className='bg-white rounded-xl p-3 shadow-md nav-btn'>
+                <FaBars />
+            </button>
         </header>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
     const inputs = "border border-black w-[15rem] h-[3rem] px-3 outline-none rounded-lg"
     return (
         <>
-            <HomeImg />
             <div className="bg-slate-300/20 flex flex-col justify-center items-center">
-                <div className="w-[70%] h-[auto] bg-white my-10 shadow-xl rounded-xl flex flex-col gap-10 justify-center items-center py-6" >
-                    <div className='flex gap-20 items-center'>
+                <HomeImg />
+                <div className="w-[70%] h-[auto] bg-white my-10 shadow-xl rounded-xl flex flex-col gap-10 justify-center items-center py-6 hotel-rev" >
+                    <div className='flex gap-20 items-center check-in-out'>
                         <div>
                             <p className='text-black p-2 font-bold'>Check in</p>
                             <input type="date" name="checkin" id="checkin" className='border border-black w-[20rem] h-[3rem] px-3 outline-none rounded-lg' />
@@ -26,7 +26,7 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className='grid grid-cols-3 gap-20'>
+                    <div className='flex gap-20 gusts'>
                         <div>
                             <p className='text-black rounded-lg p-2 font-bold'>Rooms</p>
                             <input type="number" name="rooms" id="" className={inputs} placeholder='number of rooms' />
@@ -44,23 +44,23 @@ const Home = () => {
 
                 <div className='w-full h-[auto] my-8'>
                     <div className='flex flex-col justify-center items-center'>
-                        <h1 className='text-center font-bold text-[4.5rem]'>Special Offers</h1>
-                        <div className='text-center my-2 text-lg font-[400]'>
+                        <h1 className='text-center font-bold text-[4.5rem] md:text-[3rem]'>Special Offers</h1>
+                        <div className='text-center my-2 text-lg font-[400] special-offer-para'>
                             <p>Destination can be described where you are going, Like a traveler whose</p>
                             <p>destination is Paris, Or a place that is known for a popular purpose.</p>
                         </div>
                     </div>
-                    <div className='flex justify-center items-center my-11 gap-10'>
-                        <div className='bg-white shadow-xl rounded-lg'>
+                    <div className='flex justify-center items-center my-11 gap-10 md-device'>
+                        <div className='bg-white shadow-xl rounded-lg card'>
 
-                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg'>
+                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg card-img'>
                                 <img src={resortImg} alt="" className='transition duration-300 ease-in-out hover:scale-110' />
                             </div>
 
                             <div className='p-10'>
                                 <div className='flex justify-between items-center'>
                                     <p className='text-[1.5rem] font-bold'>₹5000</p>
-                                    <p className='text-yellow-300'>offer</p>
+                                    <p className='text-yellow-300 border border-yellow-300 px-2 rounded-xl'>offer</p>
                                 </div>
                                 <div className='grid grid-cols-2 gap-4 p-4 text-center font-[500]'>
                                     <p className='bg-slate-100 shadow-lg rounded-lg py-2'><img src={bed} alt="bed" className='w-[30px]' />2 Beds</p>
@@ -78,16 +78,16 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='bg-white shadow-xl rounded-lg'>
+                        <div className='bg-white shadow-xl rounded-lg card'>
 
-                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg'>
+                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg card-img'>
                                 <img src={resortImg} alt="" className='transition duration-300 ease-in-out hover:scale-110' />
                             </div>
 
                             <div className='p-10'>
                                 <div className='flex justify-between items-center'>
                                     <p className='text-[1.5rem] font-bold'>₹5000</p>
-                                    <p className='text-yellow-300'>offer</p>
+                                    <p className='text-yellow-300 border border-yellow-300 px-2 rounded-xl'>offer</p>
                                 </div>
                                 <div className='grid grid-cols-2 gap-4 p-4 text-center font-[500]'>
                                     <p className='bg-slate-100 shadow-lg rounded-lg py-2'><img src={bed} alt="bed" className='w-[30px]' />2 Beds</p>
@@ -105,16 +105,16 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='bg-white shadow-xl rounded-lg'>
+                        <div className='bg-white shadow-xl rounded-lg card'>
 
-                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg'>
+                            <div className='w-[25rem] h-[20rem] relative overflow-hidden rounded-t-lg card-img'>
                                 <img src={resortImg} alt="" className='transition duration-300 ease-in-out hover:scale-110' />
                             </div>
 
                             <div className='p-10'>
                                 <div className='flex justify-between items-center'>
                                     <p className='text-[1.5rem] font-bold'>₹5000</p>
-                                    <p className='text-yellow-300'>offer</p>
+                                    <p className='text-yellow-300 border border-yellow-300 px-2 rounded-xl'>offer</p>
                                 </div>
                                 <div className='grid grid-cols-2 gap-4 p-4 text-center font-[500]'>
                                     <p className='bg-slate-100 shadow-lg rounded-lg py-2'><img src={bed} alt="bed" className='w-[30px]' />2 Beds</p>
@@ -134,7 +134,6 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                {/* <Footer /> */}
             </div>
         </>
     )
