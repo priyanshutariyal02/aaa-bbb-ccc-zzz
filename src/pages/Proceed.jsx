3f8b1a7ad390e236107ab050d0b488296b806fab
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-20 16:41:00 +0530

    add proceed page responsive

--- src/pages/Proceed.jsx
+++ src/pages/Proceed.jsx
@@ -4,7 +4,7 @@ const Proceed = () => {
     return (
         <div className='w-full min-h-[100vh]'>
             <div className='w-full min-h-[100vh] flex justify-center items-center'>
-                <div className='bg-green-50 w-[40rem] h-[20rem] border-2 border-green-500 flex items-center justify-center flex-col'>
+                <div className='bg-green-50 w-[40rem] h-[20rem] border-2 border-green-500 flex items-center justify-center flex-col proceed-message'>
                     <h1 className='text-[4rem] font-bold'>Thank you!</h1>
                     <p className='text-[1.3rem] font-semibold'>your booking has been successful!</p>
                     <Link to='/'>
