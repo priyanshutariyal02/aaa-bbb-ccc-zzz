559305316eeb4f728ef6bc0fc3504fe17df5e71a
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-20 18:31:57 +0530

    add proceed page responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,30 +15,33 @@ const Navbar = ({ logo }) => {
         navRef.current.classList.toggle('responsive_nav');
     };
 
+    
     return (
-        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-[115rem] mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
+        <header className='bg-black flex justify-between items-center sm:px-16 px-8 py-4 max-w-[115rem] mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'
+        
+        >
             <NavLink to="/">
                 <img src={logo} alt="logo" className='w-[233px] h-[60px] object-cover rounded-lg shadow-md home-logo' />
             </NavLink>
 
             <nav ref={navRef} className="flex text-lg gap-[10rem] font-medium md-device">
                 <div className='nav-mobile flex gap-10'>
-                    <NavLink onClick={showNavbar} exact to='/' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} exact to='/' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
                         Home
                     </NavLink>
-                    <NavLink onClick={showNavbar} exact to='/gallery' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} exact to='/gallery' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-[#ff3b65] duration-200'>
                         Gallery
                     </NavLink>
-                    <NavLink onClick={showNavbar} to='/about' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} to='/about' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
                         About
                     </NavLink>
-                    <NavLink onClick={showNavbar} to='/contact' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} to='/contact' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
                         Contact
                     </NavLink>
-                    <NavLink onClick={showNavbar} to='/booking' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} to='/booking' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
                         Booking
                     </NavLink>
-                    <NavLink onClick={showNavbar} to='/blog' className='w-[5rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
+                    <NavLink onClick={showNavbar} to='/blog' className='w-[6rem] h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md hover:text-red hover:text-[#ff3b65] duration-200'>
                         Blog
                     </NavLink>
                 </div>
