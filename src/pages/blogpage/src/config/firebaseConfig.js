b1efec82810d01eecfa13a133ecb7fa823956ecf
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-26 22:21:16 +0530

    add blogs

--- src/pages/blogpage/src/config/firebaseConfig.js
+++ src/pages/blogpage/src/config/firebaseConfig.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA6ttaIRDKxJU8TWNaXG0MuScKx45eriok",
-    authDomain: "blog-demo-5d6d1.firebaseapp.com",
-    projectId: "blog-demo-5d6d1",
-    storageBucket: "blog-demo-5d6d1.appspot.com",
-    messagingSenderId: "577086874908",
-    appId: "1:577086874908:web:841ad4d51c2be33ebac6d9"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
