9f3fcae1c67d191841da1a40338111940a26d739
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-28 20:51:19 +0530

    update

--- src/assets/icons/icon.js
+++ src/assets/icons/icon.js
@@ -10,5 +10,6 @@ import location from '../icons/location.png'
 import call from '../icons/call.png'
 import breakfast from '../icons/breakfast.png'
 import night from '../icons/night.png'
+import direction from '../icons/direction.png'
 
-export { instagram, facebook, youtube, whatsapp, bed, bath, food, bedroom, location, call, breakfast, night };
+export { instagram, facebook, youtube, whatsapp, bed, bath, food, bedroom, location, call, breakfast, night, direction };
