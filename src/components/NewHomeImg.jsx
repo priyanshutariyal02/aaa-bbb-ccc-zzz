3eefdfd2e29c55a0d781227d081ba02c90c7458f
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-28 15:05:02 +0530

    update

--- src/components/NewHomeImg.jsx
+++ src/components/NewHomeImg.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
-import { g1, g2, g6 } from "../assets/img/images.js";
+import { g1, g2, g5, g6, g8, g10 } from "../assets/img/images.js";
 const NewHomeImg = () => {
   return (
     <>
@@ -44,7 +44,7 @@ const NewHomeImg = () => {
           </Carousel.Item>
           <Carousel.Item interval={3000}>
             <img
-              src={g6}
+              src={g5}
               className="w-full h-[100vh] object-cover opacity-60"
               text="First slide"
             />
@@ -79,7 +79,77 @@ const NewHomeImg = () => {
           </Carousel.Item>
           <Carousel.Item interval={3000}>
             <img
-              src={g6}
+              src={g8}
+              className="w-full h-[100vh] object-cover opacity-60"
+              text="First slide"
+            />
+            <div className="w-full h-full absolute top-0 flex flex-col justify-center items-center text-white font-bold text-[7rem]  text-left uppercase home-heading">
+              <div className="w-full flex flex-col items-center">
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  Welcome to the
+                </h1>
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  <span className="text-[#ffee00]">Grand</span> View
+                </h1>
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  Resort
+                </h1>
+              </div>
+              <p className="font-semibold text-lg italic lowercase">
+                Stay here for peace and nautre, make memories that last forever.
+                Come to The Grand View Resort for an unforgettable experience.
+              </p>
+            </div>
+
+            <Carousel.Caption>
+              <Link to="/booking">
+                <button
+                  type="submit"
+                  className="bg-[#ffae00] text-white font-semibold px-10 py-3 rounded-lg hover:bg-[#ffc445] transition duration-200"
+                >
+                  Quick Booking
+                </button>
+              </Link>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <img
+              src={g1}
+              className="w-full h-[100vh] object-cover opacity-60"
+              text="First slide"
+            />
+            <div className="w-full h-full absolute top-0 flex flex-col justify-center items-center text-white font-bold text-[7rem]  text-left uppercase home-heading">
+              <div className="w-full flex flex-col items-center">
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  Welcome to the
+                </h1>
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  <span className="text-[#ffee00]">Grand</span> View
+                </h1>
+                <h1 className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+                  Resort
+                </h1>
+              </div>
+              <p className="font-semibold text-lg italic lowercase">
+                Stay here for peace and nautre, make memories that last forever.
+                Come to The Grand View Resort for an unforgettable experience.
+              </p>
+            </div>
+
+            <Carousel.Caption>
+              <Link to="/booking">
+                <button
+                  type="submit"
+                  className="bg-[#ffae00] text-white font-semibold px-10 py-3 rounded-lg hover:bg-[#ffc445] transition duration-200"
+                >
+                  Quick Booking
+                </button>
+              </Link>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2500}>
+            <img
+              src={g10}
               className="w-full h-[100vh] object-cover opacity-60"
               text="First slide"
             />
