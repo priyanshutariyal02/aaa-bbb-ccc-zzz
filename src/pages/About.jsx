64e730e376e97a4dcd63e0a2cb216f44361066bb
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-04-29 13:08:56 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,20 +19,13 @@ const About = () => {
     <>
       <section className="bg-gray-100 py-20 md:py-32">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-[4rem]">
-          <div className="flex w-full gap-[3rem] flex-row-reverse items-center about-section">
-            <div className="relative overflow-hidden w-[50%] about-container">
-              <img
-                src={g8}
-                alt="About Us"
-                className="rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110"
-              />
-            </div>
+          <h2 className="text-[4rem] font-bold mb-4 text-center">
+            <span className="text-gray-800">About</span>{" "}
+            <span className="text-[#ffae00]">Us</span>
+          </h2>
+          <div className="flex w-full gap-[3rem] justify-center text-center about-section mt-[4rem]">
             <div className="w-[50%] about-container">
-              <h2 className="text-[4rem] font-semibold mb-4">
-                <span className="text-gray-800">About</span>{" "}
-                <span className="text-[#ffae00]">Us</span>
-              </h2>
-              <p className="text-gray-700 leading-relaxed mb-6 py-5 text-[20px] text-justify tracking-wide  about-para">
+              <p className="text-gray-700 leading-relaxed text-[20px] text-justify tracking-wide  about-para">
                 Welcome to Grand View Resort, nestled between the picturesque
                 towns of Dhanaulti and the majestic Queen of Hills, Mussoorie!
                 Our prime location offers breathtaking vistas of the Himalayas
@@ -45,14 +38,23 @@ const About = () => {
                 at Grand View Resort, where nature's beauty awaits just beyond
                 your doorstep.
               </p>
-              <a
-                href="/"
-                className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-medium py-3 px-6 rounded-md transition-colors duration-300"
-              >
-                Home
-              </a>
+            </div>
+            <div className="relative overflow-hidden w-[50%] about-container">
+              <img
+                src={g8}
+                alt="About Us"
+                className="rounded-md shadow-lg transition duration-300 ease-in-out hover:scale-110"
+              />
             </div>
           </div>
+        </div>
+        <div className="w-full flex justify-center mt-[4rem]">
+          <a
+            href="/"
+            className=" bg-blue-500 hover:bg-blue-600 text-white text-center font-medium py-3 px-6 rounded-md transition-colors duration-300"
+          >
+            Home
+          </a>
         </div>
       </section>
     </>
